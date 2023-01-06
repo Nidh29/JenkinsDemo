cd242e4e372a6f51a4eb59af17849c3c2de90032
SachinKumar <SachinKumar@DESKTOP-CR65LGQ>
2023-01-06 22:59:08 +0530

    First Jenkins commit

---
+++ JenkinsProject/src/main/java/demo/DemoTest1.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	@Test
+	public void m1() 
+	{
+		Reporter.log("m1 Running");
+	}
+
+}
---
+++ JenkinsProject/src/main/java/demo/DemoTest2.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+@Test
+public void m2()
+{
+	Reporter.log("m2 Running");
+}
+
+}
